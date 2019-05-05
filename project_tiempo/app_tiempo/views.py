8bc7b3e62b98f7d1f647307d6c1b2970a4631f18
jcruzde <j.cruzde@alumnos.urjc.es>
2019-05-05 15:20:49 +0200

    Añado menu y gestion de /usuario

--- project_tiempo/app_tiempo/views.py
+++ project_tiempo/app_tiempo/views.py
@@ -4,29 +4,51 @@ from django.template import loader
 from django.views.decorators.csrf import csrf_exempt
 import json
 from django.contrib.auth import logout, authenticate, login
-from .forms import Form_login
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def main(request):
 
-    form_in = Form_login()
-
     if request.method == "GET":
-        return render(request, './base.html', {'form_in': form_in})
+        return render(request, './main.html')
     elif request.method == "POST":
         if len(request.POST.keys()) == 1: # Solo tiene el token
             logout(request)
-            return(render(request, './base.html', {'form_in': form_in}))
+            return(render(request, './main.html'))
         else:
             usuario = request.POST['usuario']
             contraseña = request.POST['contraseña']
             user = authenticate(username=usuario, password=contraseña)
             if user:
                 login(request, user)
-            return(render(request, './base.html', {'form_in': form_in}))
+            return(render(request, './main.html'))
 
 def usuario(request, usuario):
-    return HttpResponse('Pagina de un usuario')
+
+    # Controlar esto al haber un POST
+    try:
+        user = User.objects.get(username=usuario)
+        print('El usuario existe')
+        content = "Mi lista de municipios"
+    except User.DoesNotExist:
+        print('El usuario no existe')
+        content = "Este usuario no está registrado."
+
+    if request.method == "GET":
+        return render(request, './usuario.html')
+    elif request.method == "POST":
+        if len(request.POST.keys()) == 1: # Solo tiene el token
+            logout(request)
+            return(render(request, './usuario.html'))
+        else:
+            usuario = request.POST['usuario']
+            contraseña = request.POST['contraseña']
+            user = authenticate(username=usuario, password=contraseña)
+            if user:
+                login(request, user)
+            return(render(request, './usuario.html'))
+
 
 def municipios(request):
     return HttpResponse('Pagina de municipios')
